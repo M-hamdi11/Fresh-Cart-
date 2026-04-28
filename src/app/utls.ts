d08312682d396b3b1da6705e3937f097ce2504f1
mahmoud hamdi <mahmoudhamdiouf4@gmail.com>
2026-04-28 05:18:54 +0300

    fix cookie name for vercel

--- src/app/utls.ts
+++ src/app/utls.ts
@@ -1,18 +1,18 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export async function decodetoken(){
-const cookie=await cookies();
-const nexttoken=cookie.get("next-auth.session-token")?.value;
+export async function decodetoken() {
+  const cookie = await cookies();
+  
+  const nexttoken =
+    cookie.get("__Secure-next-auth.session-token")?.value ||
+    cookie.get("next-auth.session-token")?.value;
 
-const jwtres= await decode({token:nexttoken,secret:process.env.NEXTAUTH_SECRET!});
-if(jwtres){
+  const jwtres = await decode({ token: nexttoken, secret: process.env.NEXTAUTH_SECRET! });
+  
+  if (jwtres) {
     return jwtres.token as string;
-}
-else{
+  } else {
     return null;
-
-}
-
-
+  }
 }
